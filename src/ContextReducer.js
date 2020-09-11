7cd68a67498af46a1b863581ff8384c9a0e364e3
muhammad atique <atiquenwl@gmail.com>
2020-09-11 15:35:58 +0500

    reducer updated with default case

--- src/ContextReducer.js
+++ src/ContextReducer.js
@@ -3,8 +3,11 @@ const ContentReducer = (state, action)=>{
     switch(action){
         case 'increament':
             return state +1
-        // case 'decreament':
-        //     return state -1
+        case 'decreament':
+            return state -1
+        default:
+            break
+
     }
 }
 
