02117cc524370b617ac2216bd69d0b2cbff7c10c
muhammad atique <atiquenwl@gmail.com>
2020-09-11 07:31:00 +0500

    repo initialized

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{ useState } from 'react';
+import Parent from './parent.js';
+import valueContext from './ValueContext.js';
+
+
+//-----------Method to pass value ( app --> parent --> Child)
+// function App() {
+//   let [number ,setNumber] = useState(10)
+//   return (
+//     <div>
+//       <h1>Pasing Value  via root (APP-Parent-child)</h1>
+//       <br />
+//       <Parent num = {number}></Parent>
+//       <br />
+//       <br />
+//       <button onClick = {()=> setNumber(++number)}>Update Number</button>
+//     </div>
+//   );
+// }
 
 function App() {
+  let num = 81
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <valueContext.Provider value ={num}>
+    <div>
+      <h1>Pasing Value  via value context</h1>
+    
+      <br />
+      <Parent></Parent>
+      <br />
+      <br />
+     {/* <button onClick = {()=> setNumber(++number)}>Update Number</button> */}
     </div>
+    </valueContext.Provider>
   );
 }
 
